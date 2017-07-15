50a8ddd76a74749cf091c028d5afd0725d044a93
drubb <info@drubb.de>
2017-07-15 15:54:32 +0200

    Some structural cleanup

--- base-cli/setup.sh
+++ base-cli/setup.sh
@@ -8,18 +8,20 @@ rc-service networking start
 
 # Install some basic tools needed inside the container
 apk update && apk upgrade
+apk add curl wget git mc nano openssh-client shadow
+
+# Configure the timezone
 apk add tzdata
 cp /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 echo "Europe/Berlin" > /etc/timezone
 apk del tzdata
-apk add curl wget git mc nano openssh-client shadow
 
 # Add zsh / oh-my-zsh and make it the default shell
 apk add zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 sed -i -e 's/ash/zsh/g' /etc/passwd
 
-# Add Node.js and build tools
+# Add Node.js
 apk add nodejs
 npm -g install npm
 
