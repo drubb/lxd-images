ebb5c6c0ce21e14dc5f2eba500b4089b40a86f80
drubb <info@drubb.de>
2017-07-15 13:44:05 +0200

    Fixed a command

--- base-cli/setup.sh
+++ base-cli/setup.sh
@@ -20,7 +20,7 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 sed -i -e 's/ash/zsh/g' /etc/passwd
 
 # Add Node.js and build tools
-apk nodejs
+apk add nodejs
 npm -g install npm
 
 # Add a dummy .ssh folder to prevent ssh-agent complaints
