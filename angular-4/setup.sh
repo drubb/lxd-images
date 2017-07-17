434674d90cc3769af0dbd7a6bf73218a8f512663
drubb <info@drubb.de>
2017-07-17 10:51:21 +0200

    Fixedsome requirement / permission problems

--- angular-4/setup.sh
+++ angular-4/setup.sh
@@ -6,8 +6,12 @@ set -e
 # Wait for the network being available
 rc-service networking start
 
+# Add Python 2, it's a requirement for some angular stuff
+apk update && apk upgrade
+apk add python2
+
 # Install angular cli
-npm -g install npm @angular/cli
+npm -g install @angular/cli --unsafe-perm
 
 # Make the angular server accessible from the host // TODO: needs disable-host-check, too
 ng set defaults.serve.host 0.0.0.0 -g
