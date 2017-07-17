b398e8f590bbf0d45065c89560935f0b54bb6f47
drubb <info@drubb.de>
2017-07-17 10:51:49 +0200

    Remove nodejs, already installed

--- vuejs/setup.sh
+++ vuejs/setup.sh
@@ -6,10 +6,8 @@ set -e
 # Wait for the network being available
 rc-service networking start
 
-# Add node.js and vue cli
-apk update && apk upgrade
-apk add nodejs
-npm -g install npm vue-cli
+# Install vue cli
+npm -g install vue-cli
 
 # Cleanup
 rm -rf /var/cache/apk/* /tmp/*
