c49c1517ad92d1f490b5a48562945dbffb1ed00a
drubb <info@drubb.de>
2017-08-28 10:10:22 +0200

    Added PHP Tokenizer extension

--- drupal-8/setup.sh
+++ drupal-8/setup.sh
@@ -19,7 +19,7 @@ mysql -e "DROP DATABASE test; DELETE FROM mysql.user WHERE Host <> '%'; FLUSH PR
 apk add php7-intl php7-openssl php7-pdo_mysql php7-pdo_sqlite php7-opcache \
         php7-gd php7-json php7-xml php7-curl php7-phar php7-xdebug php7-zip php7-ctype \
         php7-mcrypt php7-bcmath php7-zlib php7-dom php7-pcntl php7-iconv php7-posix \
-        php7-mbstring php7-session
+        php7-mbstring php7-session php7-tokenizer
 
 # Add a symbolic link for the php cli binary
 ln -sf /usr/bin/php7 /usr/bin/php
