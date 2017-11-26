a71bce0bf0f8e515e53092d6bcba1ce7f749809d
drubb <info@drubb.de>
2017-11-26 16:22:46 +0100

    Enable mysql cli tools usage without credentials

--- recipes/installMariaDB.sh
+++ recipes/installMariaDB.sh
@@ -17,11 +17,13 @@ installMariaDB() {
 DBUSER=${DBUSER:-"root"}
 DBPASS=${DBPASS:-""}
 cp /config/mysql /root/.my.cnf
+sed -i "s/^user=.*/user=${DBUSER}/g" /root/.my.cnf
+sed -i "s/^password=.*/password='${DBPASS}'/g" /root/.my.cnf
 apk --update add mariadb mariadb-client
 /etc/init.d/mariadb setup
 rc-service mariadb start
-mysql -e "CREATE USER '${DBUSER}'@'%' IDENTIFIED BY '${DBPASS}';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DBUSER}'@'%' IDENTIFIED BY '${DBPASS}' WITH GRANT OPTION;"
-mysql -e "DROP DATABASE test; DELETE FROM mysql.user WHERE Host <> '%'; FLUSH PRIVILEGES;"
+mysql -uroot -e "CREATE USER '${DBUSER}'@'%' IDENTIFIED BY '${DBPASS}';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${DBUSER}'@'%' IDENTIFIED BY '${DBPASS}' WITH GRANT OPTION;"
+mysql -uroot -e "DROP DATABASE test; DELETE FROM mysql.user WHERE Host <> '%'; FLUSH PRIVILEGES;"
 rc-update add mariadb
 }
