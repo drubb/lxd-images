7fa5581868d820c77eaba9a0fa289738f5473c2a
drubb <info@drubb.de>
2017-12-11 09:15:54 +0100

    Fixed apk cache handling

--- recipes/cleanup.sh
+++ recipes/cleanup.sh
@@ -17,4 +17,5 @@ echo Final cleanup...
 command -v composer > /dev/null && composer clear-cache &> /dev/null
 command -v npm > /dev/null && npm -g cache clean --force &> /dev/null
 rm -rf /var/cache/apk /tmp > /dev/null
+setup-apkcache /var/cache/apk
 }
