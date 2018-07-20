c9537d99916978cca63d11b8717d04037a31978a
drubb <info@drubb.de>
2018-07-20 12:44:16 +0200

    Added Php FTP extension

--- recipes/installPhp5.sh
+++ recipes/installPhp5.sh
@@ -17,7 +17,7 @@ TIMEZONE=${TIMEZONE:-UTC}
 echo 'http://dl-cdn.alpinelinux.org/alpine/v3.5/community' >> /etc/apk/repositories
 apk --update add php5 php5-fpm
 apk add php5-intl php5-openssl php5-mysql php5-mysqli php5-pdo_mysql php5-pdo_sqlite php5-opcache \
-        php5-gd php5-json php5-xml php5-curl php5-phar php5-zip php5-ctype \
+        php5-gd php5-json php5-xml php5-curl php5-phar php5-zip php5-ctype php5-ftp \
         php5-mcrypt php5-bcmath php5-zlib php5-dom php5-pcntl php5-iconv php5-posix php5-apcu \
         php5-soap php5-xdebug
 ln -sf /usr/bin/php5 /usr/bin/php
--- recipes/installPhp7.sh
+++ recipes/installPhp7.sh
@@ -16,7 +16,7 @@ installPhp7() {
 TIMEZONE=${TIMEZONE:-UTC}
 apk --update add php7 php7-fpm
 apk add php7-intl php7-openssl php7-mysqli php7-pdo_mysql php7-pdo_sqlite php7-opcache \
-        php7-gd php7-json php7-xml php7-curl php7-phar php7-xdebug php7-zip php7-ctype \
+        php7-gd php7-json php7-xml php7-curl php7-phar php7-xdebug php7-zip php7-ctype php7-ftp \
         php7-mcrypt php7-bcmath php7-zlib php7-dom php7-pcntl php7-iconv php7-posix php7-apcu \
         php7-mbstring php7-session php7-simplexml php7-tokenizer php7-soap php7-xmlwriter
 ln -sf /usr/bin/php7 /usr/bin/php
