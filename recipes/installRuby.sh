0d493978a4173796f2647be0f736f605d8c278e2
drubb <info@drubb.de>
2018-07-20 11:00:39 +0200

    Simplified Ruby recipe

--- recipes/installRuby.sh
+++ recipes/installRuby.sh
@@ -16,6 +16,5 @@ installRuby() {
 
 # Install ruby development packages
 apk --update add build-base ruby ruby-dev ruby-rdoc ruby-irb libffi-dev libxml2-dev libxslt-dev zlib-dev
-gem install -N nokogiri -- --use-system-libraries
 
 }
